aa2e74adcf62920d6eb110bb0c978962ee23f439
OsvaldN <47303810+OsvaldN@users.noreply.github.com>
2019-05-24 13:23:27 -0400

    batching works now

--- Sepsis_2019_PhysioNet/main.py
+++ Sepsis_2019_PhysioNet/main.py
@@ -12,128 +12,100 @@ from torch import optim
 import math
 import gpytorch # currently unused
 from model import lstm
-from data_loader import load_from_file
+from data_loader import load_from_file #remove?
+from pytorch_data_loader import Dataset
 from driver import save_challenge_predictions
 import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
 
-#TODO: add more args, including train, etc.print([pt.shape for pt in train_data])
+#TODO: add more args, including train, etc.
 parser = argparse.ArgumentParser(description='PyTorch Example')
 parser.add_argument('--disable-cuda', action='store_true',
                     help='Disable CUDA')
 args = parser.parse_args()
 args.device = None
-if not args.disable_cuda and torch.cuda.is_available():
+if not args.disable_cuda and torch.cuda.is_available() and False: #remove false
     args.device = torch.device('cuda')
     torch.set_default_tensor_type('torch.cuda.DoubleTensor')
 else:
     args.device = torch.device('cpu')
     torch.set_default_tensor_type('torch.DoubleTensor')
 
-train_data, train_labels = load_from_file('/home/wanglab/Osvald/CinC_data/setA')
+'''remove all of these once data_loader is working '''
+#train_data, train_labels = load_from_file('/home/wanglab/Osvald/CinC_data/setA')
 #train_data, train_labels = load_from_file('/home/wanglab/Osvald/Sepsis/small_train')
 #train_data, train_labels = load_from_file('D:\Sepsis Challenge\setA')
+#train_data, train_labels = load_from_file(r'C:\Users\Osvald\Sepsis_ML\small_train')
+#train_data, train_labels = load_from_file(r'C:\Users\Osvald\Sepsis_ML\test')
+
+#dataloader prep TODO: change to match full dataset
+partition = dict([])
+partition['train'] = list(range(8))
+partition['validation'] = list(range(6,10))
 
 epochs = 10
 embedding = 40
 hidden_size = 64
 num_layers = 2
+batch_size = 4
+save_rate = 10
 
-def pt_count(labels, display=True):
-    sepsis = 0
-    total = 0
-    for pt in train_labels:
-        sepsis += np.count_nonzero(pt)
-        total += len(pt)
-    if display:
-        print('total data points: ', total)
-        print('sepsis data points:', sepsis)
-    n = len(train_data)
-    return (total/sepsis), n
+train_data = Dataset(partition['train'])
+train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
 
-ratio, n = pt_count(train_labels)
+val_data = Dataset(partition['validation'])
+val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=True)
 
-model = lstm(embedding, hidden_size, num_layers)
-model.load_state_dict(torch.load('/home/wanglab/Osvald/Sepsis/Models/lstm40_2_64/model_epoch4_A'))
-model.train()
+ratio = 10 # TODO: manually find ratio of sepsis occurence
+
+model = lstm(embedding, hidden_size, num_layers, batch_size)
+#model.load_state_dict(torch.load('/home/wanglab/Osvald/Sepsis/Models/lstm40_2_64/model_epoch4_A'))
+#model.eval()
 
 criterion = nn.BCEWithLogitsLoss(pos_weight=torch.DoubleTensor([ratio]).to(args.device))
 optimizer = optim.SGD(model.parameters(), lr=0.001)
 
-losses = np.zeros(epochs)
+train_losses = np.zeros(epochs)
+val_losses = np.zeros(epochs)
 
-# TODO: Figure out batching with different sizes
+# TODO: Figure out batching with different sizes w/o excessive padding
+# TODO: add validation tracking
+# TODO: edit loss so that it ignores -1s
 start = time.time()
 for epoch in range(epochs):
-    loss = 0
-    for i in range(n):
-        # TODO: -> move outside of loop - can't just call torch.from_numpy() on entire train_data - it is type numpy.object_
-        #          might have to loop through all seperately to convert to tensor
-        inputs = Variable(torch.from_numpy(train_data[i]).to(args.device))
-        targets = Variable(torch.from_numpy(train_labels[i]).to(args.device)).view(-1,1,1)
+    # Training
+    train_loss = 0
+    for batch, labels in train_loader:
+        # pass to GPU if available
+        batch, labels = batch.to(args.device), labels.to(args.device)
 
         optimizer.zero_grad()
-        outputs, hidden_state = model(inputs, None)
-        loss = criterion(outputs, targets)
+        outputs, hidden_state = model(batch, None)
+        loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
 
-        losses[epoch] += loss.data
-        if i == 0:
-            print('beginning epoch', i)
-        if i == int(n/20):
-            print('epoch progress: ' + str(int(n/i*100)) + '%')
+        train_losses[epoch] += loss.data / len(train_loader)
+    
+    # Validation
+    val_loss = 0
+    with torch.set_grad_enabled(False):
+        for batch, labels in val_loader:
+            # pass to GPU if available
+            batch, labels = batch.to(args.device), labels.to(args.device)
+            
+            outputs, hidden_state = model(batch, None)
+            loss = criterion(outputs, labels)
 
-    losses[epoch] = losses[epoch]/n
-    print('Epoch', epoch, 'loss:',losses[epoch])
+            val_losses[epoch] += loss.data / len(val_loader)
+
+    print('Epoch', epoch+1, 'train loss:', train_losses[epoch], 'validation loss:', val_losses[epoch])
     print('total runtime:', str(round(time.time() - start, 2)))
-    torch.save(model.state_dict(), '/home/wanglab/Osvald/Sepsis/Models/lstm40_2_64/model_epoch%s_A' % (epoch+1+4)) #remove +4
-    np.save('/home/wanglab/Osvald/Sepsis/Models/lstm40_2_64/losses', losses)
 
-plt.plot(losses)
+    #np.save('/home/wanglab/Osvald/Sepsis/Models/lstm40_2_64/losses', losses)
+    #if (epoch+1) % save_rate ==0:
+    #   torch.save(model.state_dict(), '/home/wanglab/Osvald/Sepsis/Models/lstm40_2_64/model_epoch%s_A' % (epoch+1))
+    
+plt.plot(train_losses)
+plt.plot(val_losses)
 plt.show()
-
-def get_sepsis_score(data, model):
-    x_mean = np.array([
-        83.8996, 97.0520,  36.8055,  126.2240, 86.2907,
-        66.2070, 18.7280,  33.7373,  -3.1923,  22.5352,
-        0.4597,  7.3889,   39.5049,  96.8883,  103.4265,
-        22.4952, 87.5214,  7.7210,   106.1982, 1.5961,
-        0.6943,  131.5327, 2.0262,   2.0509,   3.5130,
-        4.0541,  1.3423,   5.2734,   32.1134,  10.5383,
-        38.9974, 10.5585,  286.5404, 198.6777])
-    x_std = np.array([
-        17.6494, 3.0163,  0.6895,   24.2988, 16.6459,
-        14.0771, 4.7035,  11.0158,  3.7845,  3.1567,
-        6.2684,  0.0710,  9.1087,   3.3971,  430.3638,
-        19.0690, 81.7152, 2.3992,   4.9761,  2.0648,
-        1.9926,  45.4816, 1.6008,   0.3793,  1.3092,
-        0.5844,  2.5511,  20.4142,  6.4362,  2.2302,
-        29.8928, 7.0606,  137.3886, 96.8997])
-    c_mean = np.array([60.8711, 0.5435, 0.0615, 0.0727, -59.6769, 28.4551])
-    c_std = np.array([16.1887, 0.4981, 0.7968, 0.8029, 160.8846, 29.5367])
-
-    x = data[-1, 0:34]
-    c = data[-1, 34:40]
-    x_norm = np.nan_to_num((x - x_mean) / x_std)
-    c_norm = np.nan_to_num((c - c_mean) / c_std)
-
-    beta = np.array([
-        0.1806,  0.0249, 0.2120,  -0.0495, 0.0084,
-        -0.0980, 0.0774, -0.0350, -0.0948, 0.1169,
-        0.7476,  0.0323, 0.0305,  -0.0251, 0.0330,
-        0.1424,  0.0324, -0.1450, -0.0594, 0.0085,
-        -0.0501, 0.0265, 0.0794,  -0.0107, 0.0225,
-        0.0040,  0.0799, -0.0287, 0.0531,  -0.0728,
-        0.0243,  0.1017, 0.0662,  -0.0074, 0.0281,
-        0.0078,  0.0593, -0.2046, -0.0167, 0.1239])
-    rho = 7.8521
-    nu = 1.0389
-
-    xstar = np.concatenate((x_norm, c_norm))
-    exp_bx = np.exp(np.dot(xstar, beta))
-    l_exp_bx = pow(4 / rho, nu) * exp_bx
-
-    score = 1 - np.exp(-l_exp_bx)
-    label = score > 0.45
-
-    return score, label
---
+++ Sepsis_2019_PhysioNet/pytorch_data_loader.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+
+import numpy as np
+import torch
+from torch.utils import data
+import os, sys
+from driver import save_challenge_predictions
+
+train_dir = "D:/Sepsis Challenge/training"
+#train_dir = '/home/wanglab/Osvald/CinC_data/training_setB'
+
+def load_challenge_data(file, split=True):
+    with open(file, 'r') as f:
+        header = f.readline().strip()
+        column_names = header.split('|')
+        data = np.loadtxt(f, delimiter='|')
+    
+    if column_names[-1] != 'SepsisLabel':
+        print(file, ' does not have sepsis label')
+        return
+    elif split:
+        labels = data[:, -1]
+        column_names = column_names[:-1]
+        data = data[:, :-1]
+    else:
+        data, labels = data, None
+    
+    return data, labels
+
+def load_data(input_directory, limit=100, split=True):
+
+    # Find files.
+    files = []
+    for f in os.listdir(input_directory):
+        if os.path.isfile(os.path.join(input_directory, f)) and not f.lower().startswith('.') and f.lower().endswith('psv'):
+            files.append(f)
+
+    # TODO: implement output (Maybe scrap)
+    #if not os.path.isdir(output_directory):
+    #    os.mkdir(output_directory)
+
+    data_arr = []
+    label_arr = []
+    # Iterate over files.
+    for f in files:
+        # Load data.
+        input_file = os.path.join(input_directory, f)
+        data,labels = load_challenge_data(input_file, split)
+        data_arr.append(np.transpose(data))
+        label_arr.append(labels)
+        if len(data_arr) == limit:
+            break
+
+    return data_arr, label_arr
+
+def data_process(dataset, expand_dims=False):
+    '''
+    preprocessing - expand dims to match largest entry
+    output is shape [n, time_steps, 40] np array
+    each row is an hours worth of data
+    TODO: edit labels to match utility funciton
+          currently: 1 if past t_sepsis - 6, 0 otherwise
+    '''
+    n = len(dataset) # number of patients
+    max_len = 0
+    for i,pt in enumerate(dataset): #get max_len and remove NaN
+        if pt.shape[1] > max_len:
+            max_len = pt.shape[1]
+        np.nan_to_num(pt, copy=False) #replaces NaN with zeros
+        dataset[i] = pt.T
+
+    if not expand_dims:
+        return dataset
+
+    for i, pt in enumerate(dataset): # expand dimensions to match largest input
+        diff = max_len - pt.shape[0]
+        if diff:
+            pt = np.append(pt, np.ones((41, diff)).T * -1, axis=0)
+        pt = np.expand_dims(pt, axis=0)
+
+        #TODO: fix this very ugly workaround, here because of np flattening tendency
+        if i == 0: 
+            output = pt
+        else:
+            output = np.append(output, pt, axis=0)
+    
+    #data, labels = output[:,:,:-1], output[:,:,-1]
+    return output #data, labels
+
+def save_to_file(name, data, labels):
+    np.save(name + '_data', data)
+    np.save(name + '_labels', labels)
+
+def load_from_file(name):
+    data = np.load(name + '_data.npy', allow_pickle=True)
+    labels = np.load(name + '_labels.npy', allow_pickle=True)
+    print('\nLoaded data of shape:', data.shape)
+    print('Loaded labels of shape:', labels.shape, '\n')
+    return data, labels
+
+class Dataset(data.Dataset):
+  'Characterizes a dataset for PyTorch'
+  def __init__(self, IDs):
+        self.IDs = IDs #list of IDs in dataset
+
+  def __len__(self):
+        return len(self.IDs)
+
+  def __getitem__(self, index):
+        'Generates one sample of data'
+        # Select sample
+        ID = str(self.IDs[index])
+
+        # Load data and get label
+        # TODO: add arg for full path to data folder
+        x = torch.load('C:\\Users\\Osvald\\Sepsis_ML\\Sepsis_2019_PhysioNet\\data\\' + ID + '.pt')
+        y = x[:,-1]
+        x = x[:,:-1]
+
+        return x, y
+
+''' Load with no resizing example '''
+#train_data, train_labels = load_data(train_dir, limit=None, split=True)
+#train_data = data_process(train_data, expand_dims=False) # only tuns NaNs to zeros
+#save_to_file('/home/wanglab/Osvald/CinC_data/setB', train_data, train_labels)
+
+'''Load with resizing example''' # data shape (n, 40, max_len) labels shape (n, max_len)
+#train_data, _= load_data(train_dir, limit=10, split=False)
+#train_data = data_process(train_data, expand_dims=True)
+#print(train_data.shape)
+#print(train_labels.shape)
+'''
+for i,pt in enumerate(train_data):
+    torch.save(torch.from_numpy(pt), 'C:\\Users\\Osvald\\Sepsis_ML\\Sepsis_2019_PhysioNet\\data\\'+str(i)+'.pt')
+'''
+
+#save_to_file(r'C:\Users\Osvald\Sepsis_ML\test', train_data, train_labels)
+
+'''padding'''
+#train_data, train_labels = load_data(train_dir, limit=10, split=True)
+#print([pt.shape for pt in train_data])
+#train_data = data_process(train_data, expand_dims=True) # only tuns NaNs to zeros
+#print([pt.shape for pt in train_data])
+#lengths = [len(label) for label in train_labels]
